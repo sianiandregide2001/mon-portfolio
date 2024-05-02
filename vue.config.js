fd1104b7f2e240753afd30a4a179450ef1150eb4
sianiandregide <sianiandregide2001@yahoo.com>
2024-05-02 03:28:46 +0200

    first commit

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack');
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: true,
+      }),
+    ],
+  },
+});
